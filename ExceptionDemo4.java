e35caee7dc9b716d85421a32e0b9cd7bb05e5527
AJITHmishra <143226939+AJITHmishra@users.noreply.github.com>
2023-09-09 13:33:07 +0530

    Add files via upload

---
+++ ExceptionDemo4.java
@@ -0,0 +1,27 @@
+package ExceptionDemo1;
+
+public class ExceptionDemo4 
+{
+	public static A a1;
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Main method start");
+		
+			int arr[]={10,20,30,40};
+			System.out.println(arr[3]);
+			System.out.println(a1);
+			try
+			{
+				System.out.println(10/0);
+			}
+		
+		catch(Exception e)
+		{
+			System.out.println("Exception e block-1 is invoking");
+		}
+		System.out.println("Done");
+
+	}
+
+}
